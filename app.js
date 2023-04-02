744e00073d86a32d060188b7d0f56ee129a9723f
infinitepower18 <44692189+infinitepower18@users.noreply.github.com>
2023-04-02 21:49:33 +0100

    use access token until it expires

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const axios = require('axios')
+const express = require("express");
+const axios = require("axios");
 
 // Get environment variables
-require('dotenv').config({ path: require('find-config')('.env') })
+require("dotenv").config({ path: require("find-config")(".env") });
 
 // Initialize the app
 const app = express();
@@ -10,41 +10,84 @@ const app = express();
 // Setup server port
 var port = process.env.PORT || 8080;
 
+var accessToken = "";
+var accessExpiry = new Date(0);
+
 // Get installs of specified app ID
-app.get('/:appId/installs',function(req,res){
-    let appId = req.params.appId;
+app.get("/:appId/installs", function (req, res) {
+  let appId = req.params.appId;
+  var curDate = new Date();
+  if (curDate < accessExpiry) {
+    axios
+      .get(
+        "https://manage.devcenter.microsoft.com/v1.0/my/analytics/installs?applicationId=" +
+          appId,
+        {
+          headers: { Authorization: `Bearer ` + accessToken },
+        }
+      )
+      .then(function (response) {
+        res.json({
+          schemaVersion: 1,
+          label: "ms store downloads",
+          message:
+            response.data.Value[0].successfulInstallCount.toLocaleString(),
+          color: "brightgreen",
+          cacheSeconds: 3600,
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+        res.send("An error has occured. See logs for more information.");
+      });
+  } else {
     const params = new URLSearchParams({
-        grant_type: `client_credentials`,
-        client_id: process.env.CLIENT_ID,
-        client_secret: process.env.CLIENT_SECRET,
-        source: `https://manage.devcenter.microsoft.com`
+      grant_type: `client_credentials`,
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      source: `https://manage.devcenter.microsoft.com`,
     });
-    axios.post('https://login.microsoftonline.com/'+process.env.TENANT_ID+'/oauth2/token',params)
-    .then(function(response){
-        axios.get('https://manage.devcenter.microsoft.com/v1.0/my/analytics/installs?applicationId='+appId,{
-            headers: {'Authorization': `Bearer `+response.data.access_token}
-        })
-        .then(function(response){
+    axios
+      .post(
+        "https://login.microsoftonline.com/" +
+          process.env.TENANT_ID +
+          "/oauth2/token",
+        params
+      )
+      .then(function (response) {
+        accessToken = response.data.access_token;
+        accessExpiry = new Date(response.data.expires_on);
+        axios
+          .get(
+            "https://manage.devcenter.microsoft.com/v1.0/my/analytics/installs?applicationId=" +
+              appId,
+            {
+              headers: { Authorization: `Bearer ` + accessToken },
+            }
+          )
+          .then(function (response) {
             res.json({
-                "schemaVersion": 1,
-                "label": "ms store downloads",
-                "message": response.data.Value[0].successfulInstallCount.toLocaleString(),
-                "color": "brightgreen",
-                "cacheSeconds": 3600
-            }) 
-        })
-        .catch(function(error){
-            console.log(error)
-            res.send("An error has occured. See logs for more information.")
-        })
-    })
-    .catch(function(error){
-        console.log(error)
-        res.send("An error has occured. See logs for more information.")
-    });
-})
+              schemaVersion: 1,
+              label: "ms store downloads",
+              message:
+                response.data.Value[0].successfulInstallCount.toLocaleString(),
+              color: "brightgreen",
+              cacheSeconds: 3600,
+            });
+          })
+          .catch(function (error) {
+            console.log(error);
+            res.send("An error has occured. See logs for more information.");
+          });
+      })
+      .catch(function (error) {
+        console.log(error);
+        res.send("An error has occured. See logs for more information.");
+      });
+  }
+});
 
 // Launch app to listen to specified port
 app.listen(port, function () {
-     console.log("Server is running on port " + port);
+  console.log("Server is running on port " + port);
 });
